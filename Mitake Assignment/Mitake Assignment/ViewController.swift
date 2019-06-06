4da398d0244513027fc11a3e88cc2737376422b7
YentingChen <yeinbo0620@gmail.com>
2019-06-06 16:50:56 +0800

    new project

---
+++ Mitake Assignment/Mitake Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mitake Assignment
+//
+//  Created by Yenting Chen on 2019/6/6.
+//  Copyright © 2019年 yeinting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
