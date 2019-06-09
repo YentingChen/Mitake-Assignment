939dda3c8d1c239c5faca2d1b786e47da3416a99
YentingChen <yeinbo0620@gmail.com>
2019-06-09 16:34:08 +0800

    transfer dataType from String to Double

--- Mitake Assignment/Mitake Assignment/DataManager.swift
+++ Mitake Assignment/Mitake Assignment/DataManager.swift
@@ -12,13 +12,14 @@ class DataManager {
     
     let decoder = JSONDecoder()
     
-    func readJson(complitionHandler: (Trend)-> Void) {
+    func readJson(complitionHandler: (TrendDoubleType)-> Void) {
         
         do {
             if let file = Bundle.main.url(forResource: "trend_2201", withExtension: "json") {
                 let data = try Data(contentsOf: file)
                 let trend = try self.decoder.decode(Trend.self, from: data)
-                complitionHandler(trend)
+                let trendDoubleType = transferToDoubleType(trend: trend)
+                complitionHandler(trendDoubleType)
                 
             } else {
                 print("no file")
@@ -26,6 +27,23 @@ class DataManager {
         } catch {
             print(error.localizedDescription)
         }
+    }
+    
+    func transferToDoubleType(trend: Trend) -> TrendDoubleType{
+        var tickDoubleArray = [TickDoubleType]()
+        
+        for i in trend.root.tick {
+            
+            let tickDoubleType = TickDoubleType.init(t: Double(i.t)!, o: Double(i.o)!, h: Double(i.h)!, l: Double(i.l)!, c: Double(i.c)!, v: Double(i.v)!)
+            tickDoubleArray.append(tickDoubleType)
+        }
+        
+        let rootDoubleType = RootDoubleType.init(rc:  Double(trend.root.rc)!, cnts:  Double(trend.root.cnts)!, stk:  Double(trend.root.stk)!, c:  Double(trend.root.c)!, tp:  Double(trend.root.tp)!, bp:  Double(trend.root.bp)!, v:  Double(trend.root.v)!, tt:  Double(trend.root.tt)!, st:  Double(trend.root.st)!, tick: tickDoubleArray)
+        
+        let trendDoubleType = TrendDoubleType.init(root: rootDoubleType)
+        
+        return trendDoubleType
+        
     }
     
 }
--- Mitake Assignment/Mitake Assignment/TrendModel.swift
+++ Mitake Assignment/Mitake Assignment/TrendModel.swift
@@ -23,3 +23,21 @@ struct Tick: Codable {
     let t, o, h, l: String
     let c, v: String
 }
+
+//Double Type
+struct TrendDoubleType {
+    let root: RootDoubleType
+}
+
+struct RootDoubleType {
+    let rc, cnts, stk, c: Double
+    let tp, bp, v, tt: Double
+    let st: Double
+    let tick: [TickDoubleType]
+}
+
+struct TickDoubleType {
+    let t, o, h, l: Double
+    let c, v: Double
+}
+
