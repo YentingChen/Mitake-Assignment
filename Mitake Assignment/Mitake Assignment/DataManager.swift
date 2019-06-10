2b5ad807217fa5eba45bc9e2ec8870129d77cdc8
YentingChen <yeinbo0620@gmail.com>
2019-06-10 23:45:04 +0800

    refactor

--- Mitake Assignment/Mitake Assignment/DataManager.swift
+++ Mitake Assignment/Mitake Assignment/DataManager.swift
@@ -46,5 +46,119 @@ class DataManager {
         
     }
     
+    
+    func findMaxAndMin(trend: TrendDoubleType, hadler: (TickDoubleType, TickDoubleType) -> Void) {
+        
+        var maxValue = trend.root.c
+        var minValue = trend.root.c
+        var maxPoint: TickDoubleType?
+        var minPoint: TickDoubleType?
+        
+        for point in trend.root.tick {
+            
+            if Double(point.h) > maxValue {
+                maxValue = Double(point.h)
+                maxPoint = point
+            }
+            
+            if Double(point.l) < minValue {
+                minValue = Double(point.l)
+                minPoint = point
+            }
+            
+        }
+        hadler(maxPoint!, minPoint!)
+    }
+    
+    func makeLineArray(trend: TrendDoubleType, handler: ([[TickDoubleType]]) -> Void){
+        var bigArray = [[TickDoubleType]]()
+        let basicValue = trend.root.c
+        var isUp = false
+        var index = 0
+        var maxValue = trend.root.c
+        var minValue = trend.root.c
+
+        for point in trend.root.tick {
+            
+            if Double(point.h) > maxValue {
+                maxValue = Double(point.h)
+                
+            }
+            
+            if Double(point.l) < minValue {
+                minValue = Double(point.l)
+                
+            }
+            
+            if point.t == 1 {
+                if Double(point.c) >= basicValue {
+                    isUp = true
+                }else{
+                    isUp = false
+                }
+                
+                var array = [TickDoubleType]()
+                let origin = TickDoubleType.init(t: 0, o: basicValue, h: basicValue, l: basicValue, c: basicValue, v: 0)
+                array.append(origin)
+                array.append(point)
+                bigArray.append(array)
+                
+            }else{
+                
+                if point.c >= basicValue {
+                    
+                    if isUp == false{
+                        let array = [TickDoubleType]()
+                        bigArray.append(array)
+                        index += 1
+                        
+                        //add bigAry[index-1]
+                        //add bigAry[index]
+                        let lastPoint = bigArray[index-1][bigArray[index-1].count-1]
+                        let t = (lastPoint.t - point.t) / (point.c - lastPoint.c)
+                            * (lastPoint.c - basicValue) + lastPoint.t
+                        let zeroPoint = TickDoubleType.init(t: t, o: basicValue, h: basicValue, l: basicValue, c: basicValue, v: 0)
+                        bigArray[index-1].append(zeroPoint)
+                        bigArray[index].append(zeroPoint)
+                        
+                    }
+                    
+                    //add point
+                    //...
+                    bigArray[index].append(point)
+                    
+                    isUp = true;
+                }
+                else {
+                    
+                    if isUp == true{
+                        let array = [TickDoubleType]()
+                        bigArray.append(array)
+                        index += 1
+                        
+                        
+                        let lastPoint = bigArray[index-1][bigArray[index-1].count-1]
+                        let t = (lastPoint.t - point.t) / ( point.c - lastPoint.c)
+                            * (lastPoint.c - basicValue) + lastPoint.t
+                        let zeroPoint = TickDoubleType.init(t: t, o: basicValue, h: basicValue, l: basicValue, c: basicValue, v: 0)
+                        bigArray[index-1].append(zeroPoint)
+                        bigArray[index].append(zeroPoint)
+                    }
+                    
+                    //add point
+                    //...
+                    bigArray[index].append(point)
+                    
+                    isUp = false;
+                }
+            }
+        }
+        
+        let zeroPoint = TickDoubleType.init(t: Double(trend.root.tick[trend.root.tick.count-1].t + 1), o: basicValue, h: basicValue, l: basicValue, c: basicValue, v: 0)
+        bigArray[bigArray.count-1].append(zeroPoint)
+        
+        handler(bigArray)
+    }
+    
 }
 
